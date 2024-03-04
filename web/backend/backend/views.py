920db2213704ad3b65772c9e7374f914fd4b5b39
Jerry <gao1021229073@163.com>
2024-03-04 15:31:35 +0800

    backend calling qiling succeed.

--- web/backend/backend/views.py
+++ web/backend/backend/views.py
@@ -5,11 +5,38 @@ from django.http import JsonResponse
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
+#Dynamic load dev version of qiling
 import sys
-sys.path.append("E:\Projects\scriptmalsolver\qiling")
-from qiling import * 
+from importlib import util
+from os.path import basename
+from os.path import exists
+from types import ModuleType
 
-import json
+
+def load_package_from_path(pkg_path: str) -> ModuleType:
+    """
+    ref: https://stackoverflow.com/a/50395128
+    """
+    try:
+        init_path = f'{pkg_path}/__init__.py'
+        assert exists(init_path)
+        name = basename(pkg_path)
+        
+        spec = util.spec_from_file_location(name, init_path)
+        module = util.module_from_spec(spec)
+        sys.modules[spec.name] = module
+        spec.loader.exec_module(module)
+    except:
+        return None
+    return module
+
+load = load_package_from_path  # short alias
+qiling = load(r'E:\Projects\scriptmalsolver\qiling\qiling')
+
+using_public_code = False
+if(qiling == None):
+    using_public_code = True
+    from qiling import *
 
 @csrf_exempt
 def upload_file(request):
@@ -74,9 +101,8 @@ def hello_world(request):
 def test_qiling(request):
     argv = [r'E:\Projects\scriptmalsolver\rootfs\x8664_windows\bin\hello.exe']
     rootfs = r'E:\Projects\scriptmalsolver\rootfs\x8664_windows'
-
-    result = ""
-    ql = Qiling(argv=argv, rootfs=rootfs, log_file=result)
+    #verbose = qiling.QL_VERBOSE.DEBUG
+    ql = qiling.Qiling(argv=argv, rootfs=rootfs, log_file = "log_qiling.log", verbose = 4)
+    #ql = qiling.Qiling(argv=argv, rootfs=rootfs)
     ql.run()
-    return JsonResponse({'module_name':Qiling.__name__,
-                         'result' : result})
+    return JsonResponse({'result' : 'OK'})
