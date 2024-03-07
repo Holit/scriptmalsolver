3b1812296f9f6a6bffe776fcf0e58a1035961e36
Jerry <gao1021229073@163.com>
2024-03-07 14:45:06 +0800

    Updated README

--- start.bat
+++ start.bat
@@ -2,7 +2,8 @@ echo 启动测试环境...
 cd web/backend
 echo 启动Django后端
 start "Django Backend Terminal" cmd /k  py manage.py runserver
-cd ..
-cd frontend
-echo 启动vue前端
-start "Vue Frontend Terminal" cmd /k  npm run dev
+
+REM cd ..
+REM cd frontend
+REM echo 启动vue前端
+REM start "Vue Frontend Terminal" cmd /k  npm run dev
